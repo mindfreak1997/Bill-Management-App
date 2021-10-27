bc7d4b62527beb0cac8e9ea19dd3d83dc81669e6
Akhilesh Rao U <76146734+mindfreak1997@users.noreply.github.com>
2021-10-27 19:17:12 +0530

    delete extra

--- src/src/components/helper/helperBill.js
+++
@@ -1,9 +0,0 @@
-
-
-export const selectCustomer = (customers) => {
-    const result = []
-    customers.forEach(ele => {
-        result.push({ label: ele.name, value: ele.name })
-    })
-    return result
-}
